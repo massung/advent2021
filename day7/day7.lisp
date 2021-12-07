dfb9688db761353dd4406e9b1c52806cb5f6968a
Jeffrey Massung <massung@gmail.com>
2021-12-07 00:54:44 -0600

    Running standard deviation calculation for fun.

--- day7/day7.lisp
+++ day7/day7.lisp
@@ -9,16 +9,27 @@
       (floor n 2)
     (+ (* (1+ n) q) (if (zerop r) 0 (1+ q)))))
 
-(defun steps (xs n cost)
-  (reduce #'(lambda (a x) (+ a (funcall cost (abs (- x n))))) xs :initial-value 0))
+(defun stddev (xs &aux (d 0) (m 0) (n 0))
+  (dolist (x xs (values (sqrt (/ d (1- n))) m))
+    (incf n)
+    (let ((v (- x m)))
+      (incf m (/ v n))
+      (incf d (* v (- x m))))))
 
-(defun run (xs &optional (cost #'identity))
-  (loop for i below (apply #'max xs) minimizing (steps xs i cost)))
+(defun fuel-used (xs n cost)
+  (loop for x in xs sum (funcall cost (abs (- x n)))))
+
+(defun run (xs &key (cost #'identity) (z 0.5))
+  (multiple-value-bind (sd m)
+      (stddev xs)
+    (let ((a (floor (- m (* z sd))))
+          (b (floor (+ m (* z sd)))))
+      (loop for i from a to b minimizing (fuel-used xs i cost)))))
 
 (defun part-1 (&optional (data #'test-data))
   (let ((positions (first (funcall data #'parse-positions))))
-    (run positions)))
+    (time (run positions))))
 
 (defun part-2 (&optional (data #'test-data))
   (let ((positions (first (funcall data #'parse-positions))))
-    (time (run positions #'summation))))
+    (time (run positions :cost #'summation))))
