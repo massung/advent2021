005bbf342ec2b21a1e96457b9cc3a266d58ed029
Jeffrey Massung <massung@gmail.com>
2021-12-07 00:18:45 -0600

    Day 7.

--- day7/day7.lisp
+++ day7/day7.lisp
@@ -1,10 +1,24 @@
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (load "/Users/jeff/Projects/advent/common/common.lisp"))
 
+(defun parse-positions (s)
+  (mapcar #'parse-integer (re:split-re "," s :all t)))
+
+(defun summation (n)
+  (multiple-value-bind (q r)
+      (floor n 2)
+    (+ (* (1+ n) q) (if (zerop r) 0 (1+ q)))))
+
+(defun steps (xs n cost)
+  (reduce #'(lambda (a x) (+ a (funcall cost (abs (- x n))))) xs :initial-value 0))
+
+(defun run (xs &optional (cost #'identity))
+  (loop for i below (apply #'max xs) minimizing (steps xs i cost)))
+
 (defun part-1 (&optional (data #'test-data))
-  (let ((lines (funcall data #'read-from-string)))
-    nil))
+  (let ((positions (first (funcall data #'parse-positions))))
+    (run positions)))
 
 (defun part-2 (&optional (data #'test-data))
-  (let ((lines (funcall data #'read-from-string)))
-    nil))
+  (let ((positions (first (funcall data #'parse-positions))))
+    (time (run positions #'summation))))
